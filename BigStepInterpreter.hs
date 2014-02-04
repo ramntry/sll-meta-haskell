b1a1f455004e62ac5bb63dfa26e9002fbc5a84ff
ramntry <ramntry@gmail.com>
2014-02-04 11:07:05 +0400

    remove redundant calculations from big step interpreter

--- BigStepInterpreter.hs
+++ BigStepInterpreter.hs
@@ -8,11 +8,11 @@ int :: Program -> Expr -> Expr
 
 int p (Ctr name args) = Ctr name (map (int p) args)
 
-int p (FCall name args) = int p (body // zip vs (map (int p) args))
+int p (FCall name args) = int p (body // zip vs args)
   where (FDef _ vs body) = fDef p name
 
 int p (GCall gname (Ctr cname cargs : args)) =
-  int p (body // zip (cvs ++ vs) (map (int p) (cargs ++ args))) where
+  int p (body // zip (cvs ++ vs) (cargs ++ args)) where
     (GDef _ (Pat _ cvs) vs body) = gDef p gname cname
 
 int p (GCall gname (pat : args)) =
